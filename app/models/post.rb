a9523c03fc8c96b6f92078b0016faaa7dfcc6e64
shawn <173310929+shawntarou@users.noreply.github.com>
2026-09-12 14:26:30 -0700

    Setup Post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :title, :body, length: { minimum: 1, maximum: 100 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :posts
+
+  # if user is deleted, all associated posts will be deleted as well
+  has_many :posts, dependent: :destroy
 end
