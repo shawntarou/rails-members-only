cf9a6d90de5e6c14a1881df746f28ed44ed4c38a
shawn <173310929+shawntarou@users.noreply.github.com>
2026-09-12 14:36:49 -0700

    Posts now link to show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,25 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[ show edit update destroy ]
+
   def index
     @posts = Post.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_post
+      @post = Post.find(params.expect(:id))
   end
 end
