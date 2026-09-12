7e62c6811a4540f30b4b29e00bb2c2bf50405f7f
shawn <173310929+shawntarou@users.noreply.github.com>
2026-09-12 15:28:39 -0700

    Feat: signed in users can see author of post

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :posts
   # Defines devise signup links, login links, edit profile links, etc.
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions"
+  }
   # Defines the root path route ("/")
   root "posts#index"
 end
