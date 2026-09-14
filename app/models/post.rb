192f387f05f745a7ca6e4863febfe64c1f37f042
shawn <173310929+shawntarou@users.noreply.github.com>
2026-09-13 20:22:36 -0700

    Add temporary styles

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validates :title, :body, length: { minimum: 1, maximum: 100 }
+  validates :title, :body, length: { minimum: 1, maximum: 1000 }
 end
